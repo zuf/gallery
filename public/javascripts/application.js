d0067ccc8b56acefb5f56704e30989c9bdc68de7
Johan Sahlén <johan.sahlen@gmail.com>
2009-05-06 22:23:27 +0200

    Better, but not perfect, lightbox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,12 +5,23 @@
   });
 
   $.fn.lightbox = function(options) {
-    var show = function() {
-      redraw();
+    var options = $.extend({
+      padding: 5
+    }, options);
 
+    var show = function() {
       container.show().animate({ opacity: 1 }, 200);
 
-      return false;
+      win.bind('scroll.lightbox', redraw);
+      win.bind('keyup.lightbox', function(e) {
+        if (working) return;
+
+        if (e.keyCode == 37) loadPrevious();
+        if (e.keyCode == 39) loadNext();
+        if (e.keyCode == 27) hide();
+
+        return false;
+      });
     };
 
     var create = function() {
@@ -24,13 +35,26 @@
       $('body').append(backdrop).append(container);
     };
 
-    var redraw = function() {
-      backdrop.css('height', doc.height()).css('width', doc.width());
+    var redraw = function(imgWidth, imgHeight) {
+      if (!backdrop.is(':visible')) backdrop.css('height', doc.height()).css('width', doc.width());
+
+      var img = container.find('img:visible');
+      if (img.length) {
+        imgWidth = img.width();
+        imgHeight = img.height();
+      }
+
+      if (imgWidth && imgHeight) {
+        container.css({
+          left: (doc.width()-imgWidth+(options.padding*2))/2,
+          top: ((win.height()-imgHeight-(options.padding*2))/2)+win.scrollTop()
+        });
+      }
     };
 
-    var load = function(src) {
-      var link = $(this);
-      var src = link.attr('href');
+    var load = function(src, index) {
+      currentIndex = index;
+      working = true;
 
       create();
       redraw();
@@ -45,22 +69,56 @@
         } else {
           container.prepend(newImg);
         }
-      
-        show();
+
+        var fakeImg = newImg.clone().css('opacity', 0).attr('id', 'lightbox_fake').appendTo('body');
+        fakeImg.load(function() {
+          redraw(fakeImg.width(), fakeImg.height())
+          show();
+          fakeImg.remove();
+          working = false;
+        });
       });
 
       return false;
     };
 
+    var loadPrevious = function() {
+      if (currentIndex > 0) {
+        var link = links.slice(currentIndex-1, currentIndex);
+        var src = link.attr('href');
+        load(src, currentIndex-1);
+      }
+    };
+
+    var loadNext = function() {
+      if (currentIndex < links.length) {
+        var link = links.slice(currentIndex+1, currentIndex+2);
+        var src = link.attr('href');
+        load(src, currentIndex+1);
+      }
+    };
+
     var hide = function() {
       backdrop.animate({ opacity: 0 }, 100, null, function() { backdrop.hide(); });
       container.animate({ opacity: 0 }, 100, null, function() { container.hide(); });
+      win.unbind('scroll.lightbox').unbind('keyup.lightbox');
     };
 
     var container, backdrop;
     var doc = $(document);
+    var win = $(window);
     var links = this;
-    links.click(load);
+    var currentIndex = 0;
+    var working = false;
+
+    links.each(function(index) {
+      var link = $(this);
+      var src = link.attr('href');
+      link.click(function() {
+        load(src, index);
+        return false;
+      });
+    });
   };
 
 }(jQuery));
